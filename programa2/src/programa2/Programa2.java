7ef97dae37bef8e23eadb2f77cb84a1e7a0fdc5f
Bocadin <ma202524453@alm.buap.mx>
2026-04-16 09:25:07 -0600

    Add files via upload

---
+++ programa2/src/programa2/Programa2.java
@@ -0,0 +1,16 @@
+
+package programa2;
+
+public class Programa2 {
+
+    public static void main(String[] args) {
+        double base = 5.55;
+        double altura = 10.5; 
+        
+        double area = (base * altura) / 2;
+        
+        System.out.println("La area de un triangulo es: " + area);
+        System.out.printf("El area del triangulo es: %.3f \n " , area );
+    }
+    
+}
