8337f2d109d3ccd7f1589ce1a10da40bfa7ec246
Bocadin <ma202524453@alm.buap.mx>
2026-04-16 09:35:51 -0600

    Add files via upload

---
+++ programa3/src/programa3/Programa3.java
@@ -0,0 +1,13 @@
+package programa3;
+import java.util.Scanner;
+public class Programa3 {
+
+    public static void main(String[] args) {
+         Scanner lector = new Scanner (System.in);
+         
+         System.out.println(" Que carrera estudias?:  ");
+         String carrera = lector.nextLine();
+         System.out.println("La carrera que estudias es: ");
+    }
+    
+}
