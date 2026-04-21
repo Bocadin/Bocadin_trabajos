d9a4dda82d04e26d74a71515e6d791017e200a3d
Bocadin <ma202524453@alm.buap.mx>
2026-04-21 08:45:12 -0600

    Add files via upload

---
+++ Ejercicios1Jav/src/ejercicios1jav/Ejercicios1Jav.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios1jav;
+
+/**
+ *
+ * @author 240019110
+ */
+public class Ejercicios1Jav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
